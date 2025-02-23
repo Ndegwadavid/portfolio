1d8d3a3998e92803a06c97d1df7fb701fbb46855
Ndegwadavid <davidndegwa6869@gmail.com>
2025-02-23 16:52:16 +0300

    Update: project image

--- src/app/resources/config.js
+++ src/app/resources/config.js
@@ -11,7 +11,7 @@ const routes = {
 // Enable password protection on selected routes
 // Set password in the .env file, refer to .env.example
 const protectedRoutes = {
-  "/work/automate-design-handovers-with-a-figma-to-code-pipeline": true,
+  "/work/Enterprise-client-management-system": false, //cahnge to true when building ore scalable portfolio
 };
 
 const style = {
